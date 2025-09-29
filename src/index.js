2b59b5e4fa0baa99ce595b4a839c4f76ce2a0d20
Dan Course <hello@dancourse.co.uk>
2025-09-29 08:38:12 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import SpellingGridGenerator from './App';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <SpellingGridGenerator />
